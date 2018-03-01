8c8d6e01e775919bd04c89e7b92083283812ef9b
epic8009 <ilikechips84@gmail.com>
2018-03-01 22:18:47 +0000

    New title animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,20 @@
-document.onload=function(){
-	document.getElementById("game").style.display="";
-}
+var game=undefined;
 var titleFrames=[
-	"/\\Boring Incremental\\/",
-	"\\/Boring Incremental/\\"
+	"-==Boring Incremental==-",
+	"--=Boring Incremental=--",
+	"---Boring Incremental---",
+	"--=Boring Incremental=--",
+	"-==Boring Incremental==-",
+	"===Boring Incremental==="
 ];
 var titleFrame=0;
-setInterval(()=>{
-	titleFrame++;
-	document.title=titleFrames[titleFrame%titleFrames.length];
-},1000/2);
+document.onload=function(){
+	game=document.getElementById("game");
+	game.style.display="";
+	setInterval(()=>{
+		titleFrame++;
+		document.title=titleFrames[titleFrame%titleFrames.length];
+		
+	},1000/2);
+	
+}
