b276c20200ce8d0813cdd76105f3562f12ef8ac9
epic8009 <ilikechips84@gmail.com>
2018-03-02 19:49:44 +0000

    we now have currency biotch

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 var game=undefined;
+var tickspeed=30;
+var chocolateBarDec=0.0;
+var actualChocolate=0;
 var titleFrames=[
 	"-==Boring Incremental==-",
 	"--=Boring Incremental=--",
@@ -8,13 +11,22 @@ var titleFrames=[
 	"===Boring Incremental===" //aaaaaaaaaaaaaaa
 ];
 var titleFrame=0;
+function addChocolate(amount){
+	chocolateBarDec+=amount;
+	actualChocolate=Math.floor(chocolateBarDec);
+}
+var dts=new Date().getTime();
 window.onload=function(){
+	var dt=new Date().getTime()-dts;
+	dts=new Date().getTime();
+	addChocolate(1/(tickspeed/dt));
 	game=document.getElementById("game");
 	game.style.display="";
 	setInterval(()=>{
 		titleFrame++;
 		document.title=titleFrames[titleFrame%titleFrames.length];
-		
 	},1000/2);
-	
+	setInterval(()=>{
+		
+	},1000/tickspeed);
 }
