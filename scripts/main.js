cd2f99629e98aac38ff3a2ebd66f66d1282bc364
chinese-gihub <37048595+chinese-gihub@users.noreply.github.com>
2018-03-04 20:26:53 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,30 @@
-var game=undefined;
-var tickspeed=30;
-var chocolateBarDec=0.0;
-var actualChocolate=0;
-var titleFrames=[
+/* main.js
+ *
+ * Copyright (C) 2018 epic8009, Leaf
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
+var game = undefined;
+var tickspeed = 30;
+var chocolateBarDec = 0.0;
+var actualChocolate = 0;
+var titleFrame = 0;
+var dts = calcDts ();
+var titleFrames = [
 	"-==Boring Incremental==-",
 	"--=Boring Incremental=--",
 	"---Boring Incremental---",
@@ -10,23 +32,28 @@ var titleFrames=[
 	"-==Boring Incremental==-",
 	"===Boring Incremental===" //aaaaaaaaaaaaaaa
 ];
-var titleFrame=0;
-function addChocolate(amount){
-	chocolateBarDec+=amount;
-	actualChocolate=Math.floor(chocolateBarDec);
+function calcDts () {
+	return (new Date ().getTime ());
 }
-var dts=new Date().getTime();
-window.onload=function(){
-	var dt=new Date().getTime()-dts;
-	dts=new Date().getTime();
-	addChocolate(1/(tickspeed/dt));
-	game=document.getElementById("game");
-	game.style.display="";
+function addChocolate (amount) {
+	chocolateBarDec += amount;
+	actualChocolate = Math.floor (chocolateBarDec);
+}
+onload = function () {
+	var dt = (new Date ().getTime ()) - dts;
+	var yourChocoIsMyMotto = document.createElement ("p");
+	document.removeChild (document.getElementById ('nojs'));
+	dts = calcDts ();
+	addChocolate (1 / (tickspeed / dt));
+	game = document.getElementById ("game");
+	game.style.display = "";
+	game.appendChild (yourChocoIsMyMotto);
 	setInterval(()=>{
-		titleFrame++;
-		document.title=titleFrames[titleFrame%titleFrames.length];
+		titleFrame ++;
+		document.title = titleFrames[titleFrame % titleFrames.length];
 	},1000/2);
 	setInterval(()=>{
 		
 	},1000/tickspeed);
+	yourChocoIsMyMotto.innerText = "Actual Chocolate: " + actualChocolate;
 }
