07119fa20a352f0f3841bb00e9e0aad1a651fc31
epic8009 <ilikechips84@gmail.com>
2018-03-05 13:57:22 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -42,7 +42,6 @@ function addChocolate (amount) {
 onload = function () {
 
 	var yourChocoIsMyMotto = document.createElement ("p");
-	document.removeChild (document.getElementById ('nojs'));
 
 	game = document.getElementById ("game");
 	game.style.display = "";
